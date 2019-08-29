085782f345c496319b7870162216f5c2dd67fdb6
perlancar (on pc-office) <perlancar@gmail.com>
2019-08-29 21:44:51 +0700

    [bugfix] Fix ordering of records that's demanded by bind: NS first, then CNAME *right* before a wildcard, then the others

--- lib/DNS/Zone/PowerDNS/To/BIND.pm
+++ lib/DNS/Zone/PowerDNS/To/BIND.pm
@@ -86,17 +86,49 @@ sub gen_bind_zone_from_powerdns_db {
     my @res;
     push @res, '; generated from PowerDNS database on '.scalar(gmtime)." UTC\n";
 
-    my $sth_sel_soa_record = $dbh->prepare("SELECT * FROM records WHERE domain_id=? AND disabled=0 AND type='SOA'");
-    $sth_sel_soa_record->execute($domain_rec->{id});
-    my $soa_rec = $sth_sel_soa_record->fetchrow_hashref
-        or die "Domain '$domain' does not have SOA record";
-    push @res, '$TTL ', $soa_rec->{ttl}, "\n";
-    $soa_rec->{content} =~ s/(\S+)(\s+)(\S+)(\s+)(.+)/$1.$2$3.$4($5)/;
-    push @res, "\@ IN $soa_rec->{ttl} SOA $soa_rec->{content};\n";
+    my $soa_rec;
+  GET_SOA_RECORD: {
+        my $sth_sel_soa_record = $dbh->prepare("SELECT * FROM records WHERE domain_id=? AND disabled=0 AND type='SOA'");
+        $sth_sel_soa_record->execute($domain_rec->{id});
+        $soa_rec = $sth_sel_soa_record->fetchrow_hashref
+            or die "Domain '$domain' does not have SOA record";
+        push @res, '$TTL ', $soa_rec->{ttl}, "\n";
+        $soa_rec->{content} =~ s/(\S+)(\s+)(\S+)(\s+)(.+)/$1.$2$3.$4($5)/;
+        push @res, "\@ IN $soa_rec->{ttl} SOA $soa_rec->{content};\n";
+    }
 
-    my $sth_sel_record = $dbh->prepare("SELECT * FROM records WHERE domain_id=? AND disabled=0 ORDER BY id");
-    $sth_sel_record->execute($domain_rec->{id});
-    while (my $rec = $sth_sel_record->fetchrow_hashref) {
+    my @recs;
+  GET_RECORDS:
+    {
+        my $sth_sel_record = $dbh->prepare("SELECT * FROM records WHERE domain_id=? AND disabled=0 ORDER BY id");
+        $sth_sel_record->execute($domain_rec->{id});
+        while (my $rec = $sth_sel_record->fetchrow_hashref) {
+            push @recs, $rec;
+        }
+
+        # bind requires some particular ordering of records...
+        @recs = sort {
+            my $cmp;
+
+            # NS records first ...
+            my $a_is_ns = $a->{type} eq 'NS' ? 0 : 1;
+            my $b_is_ns = $b->{type} eq 'NS' ? 0 : 1;
+            return $cmp if $cmp = $a_is_ns <=> $b_is_ns;
+
+            # we need to sort wildcard records RIGHT above non-wildcard records;
+            # otherwise we might get, e.g. 'CNAME and other data' error, where
+            # there is '* A' or '* MX', followed by 'www CNAME' which will be
+            # rejected by bind because www gets CNAME as well as A or MX, while
+            # CNAME cannot be mixed with other record types.
+            my $a_cname_and_wildcard_score = $a->{type} eq 'CNAME' ? 1 : $a->{name} =~ /\*/ ? 2 : 3;
+            my $b_cname_and_wildcard_score = $b->{type} eq 'CNAME' ? 1 : $b->{name} =~ /\*/ ? 2 : 3;
+            return $cmp if $cmp = $a_cname_and_wildcard_score <=> $b_cname_and_wildcard_score;
+
+            $a->{name} cmp $b->{name};
+        } @recs;
+    }
+
+    for my $rec (@recs) {
         my $type = $rec->{type};
         next if $type eq 'SOA';
         my $name = $rec->{name};
